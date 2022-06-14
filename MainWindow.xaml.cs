55633f282fe8f00481c412759847fae33cc5441f
student 04 <stk_lum_04@outlook.de>
2022-06-14 12:56:40 +0200

    Align - funktioniert noch nicht

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,7 +27,7 @@ namespace Texteditor
         {
             InitializeComponent();
             cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source); //hier geben wir Combobox die Einträge
-            cmbFontsize.ItemsSource = new List<double>() { 8,9,10,11,12,14,16,18,20,22,24,26,28,36,48,72}; //geben die default Größen ein
+            cmbFontsize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 }; //geben die default Größen ein
         }
 
         private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -46,15 +47,19 @@ namespace Texteditor
             object temp = rtbEditor.Selection.GetPropertyValue(Inline.FontWeightProperty); //"Weight" entspricht "bold"
             btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && temp.Equals(FontWeights.Bold); //brauchen das, damit ein Programm erkennt, ob in Selection etw bold usw ist
 
-            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty); 
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontStyleProperty);
             btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && temp.Equals(FontStyles.Italic); //"Style" entspricht "italic"
 
             temp = rtbEditor.Selection.GetPropertyValue(Inline.TextDecorationsProperty); //"Style" entspricht "bold"
             btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && temp.Equals(TextDecorations.Underline);
 
 
-            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty); //Schrift aendern
-            cmbFontFamily.SelectedItem = temp; //setzen ausgewaehlte Wert in Zeile
+
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontFamilyProperty); //Schrift ändern
+            cmbFontFamily.SelectedItem = temp; //setzen ausgewählte Wert in Zeile
+
+            temp = rtbEditor.Selection.GetPropertyValue(Inline.FontSizeProperty); //Größe der Schriftes ändern
+            cmbFontsize.Text = temp.ToString(); //setzen ausgewählte Wert in Zeile, wobei zu beachten ist, dass hier man .Text und .ToString() verwendet, weil wir keine bereite Werte haben
         }
 
         private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
@@ -77,13 +82,37 @@ namespace Texteditor
             }
         }
 
-        private void cmbFontsize_TextChanged(object sender, RoutedEventArgs e) //die Methode für Groesse
+        private void cmbFontsize_TextChanged(object sender, TextChangedEventArgs e) //die Methode für Groesse
         {
-            //if (cmbFontsize.SelectedItem != null) //wenn etwas ausgewählt ist 
-            //{
-            //    rtbEditor.Selection.ApplyPropertyValue(Inline.FontsizeProperty, cmbFontFamily.SelectedItem);
-            //}
+            double a;
+            if (cmbFontsize.SelectedItem != null) //wenn etwas ausgewählt ist 
+            {
+                rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontsize.SelectedItem);
+            }
+            else if (cmbFontsize.Text == "")
+            {
+                return;
+            }
+            else if (!Double.TryParse(cmbFontsize.Text, out a))
+            {
+                MessageBox.Show("Dies ist keine gültige Zahl","TextEditor");
+                
+                return;
+            }
+            else if (Convert.ToDouble(cmbFontsize.Text) == 0)
+            {
+                MessageBox.Show("Die Zahl muss größer als 0 sein","TextEditor");
+                return;
+            }
+            else
+            {
+                rtbEditor.Selection.ApplyPropertyValue(Inline.FontSizeProperty, cmbFontsize.Text);
+            }
         }
-        
+
+        private void cmbFontsize_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"^[0-9]*(\,)?$");
+        }
     }
 }
